645ae6a07b40768469fe8634c68b9eeb72d66a66
havana723 <71638185+havana723@users.noreply.github.com>
2022-06-05 21:32:16 +0900

    add api

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,42 @@
+import bodyParser from "body-parser";
 import express from "express";
-import { logs } from "./logs/log";
+import { History } from "src/types/history";
 
 const app = express();
+const fs = require("fs");
 const port = process.env.PORT ?? 3001;
 
+app.use(bodyParser.json());
+
 app.get("/api/howmany", (req, res) => {
+  const logs = JSON.parse(
+    fs.readFileSync(__dirname + "/log.txt").toString()
+  ) as unknown as History[];
   res.send({ cnt: logs[logs.length - 1].cnt });
 });
 
 app.get("/api/lastupdate", (req, res) => {
+  const logs = JSON.parse(
+    fs.readFileSync(__dirname + "/log.txt").toString()
+  ) as unknown as History[];
   res.send({ update: logs[logs.length - 1].update });
+});
+
+app.post("/api/history", (req, res) => {
+  const logs = JSON.parse(
+    fs.readFileSync(__dirname + "/log.txt").toString()
+  ) as unknown as History[];
+
+  const history: History = {
+    cnt: req.body.cnt,
+    update: new Date(),
+  };
+
+  logs.push(history);
+
+  fs.writeFileSync(__dirname + "/log.txt", JSON.stringify(logs));
+
+  res.send(201);
 });
 
 app.listen(port, () => {
