62bec3ce4398d212d3ff6bcba191e059bd76ac57
Suho Lee <bird@susuyo.ai>
2022-06-07 17:48:44 +0900

    refactor: Remove redundant types

--- src/app.ts
+++ src/app.ts
@@ -20,21 +20,21 @@ app.use(
 app.get("/api/howmany", (req, res) => {
   const logs = JSON.parse(
     fs.readFileSync(__dirname + "/log.txt").toString()
-  ) as unknown as History[];
+  ) as History[];
   res.send({ cnt: logs[logs.length - 1].cnt });
 });
 
 app.get("/api/lastupdate", (req, res) => {
   const logs = JSON.parse(
     fs.readFileSync(__dirname + "/log.txt").toString()
-  ) as unknown as History[];
+  ) as History[];
   res.send({ update: logs[logs.length - 1].update });
 });
 
 app.post("/api/history", (req, res) => {
   const logs = JSON.parse(
     fs.readFileSync(__dirname + "/log.txt").toString()
-  ) as unknown as History[];
+  ) as History[];
 
   const history: History = {
     cnt: req.body.cnt,
